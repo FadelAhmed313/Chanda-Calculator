4873102860bf69bf9a2bb6a0aa8d022c4b9f9611
FadelAhmed313 <146953610+FadelAhmed313@users.noreply.github.com>
2024-04-03 23:44:49 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,14 @@
 self.addEventListener('push', (event) => {
+  const data = event.data.json(); // Parse the push message data (if it's in JSON format)
+  const title = data.title || 'Chanda Calculator Notification'; // Get the title from the data or use a default title
+
   const options = {
-    body: event.data.text(), // Get the push message text
-    //icon: '/path/to/icon.png', // Set an icon for the notification
-    //badge: '/path/to/badge.png', // Set a badge for the notification
+    body: data.body || 'Please pay your chanda for the current month', // Get the notification body (or use a default)
+    icon: 'ico.png', // Set an icon for the notification
   };
 
   event.waitUntil(
-    self.registration.showNotification('ChandaApp', options)
+    self.registration.showNotification(title, options)
   );
 });
+
