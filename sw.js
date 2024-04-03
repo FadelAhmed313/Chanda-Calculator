b4bdcb1ecedcdc4414bd75b80c6414155eec250b
FadelAhmed313 <146953610+FadelAhmed313@users.noreply.github.com>
2024-04-04 00:27:05 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
+// Service Worker setup
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                'index.html'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
 self.addEventListener('push', (event) => {
   const data = event.data.json(); // Parse the push message data (if it's in JSON format)
   const title = data.title || 'Chanda Calculator Notification'; // Get the title from the data or use a default title
