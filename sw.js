3735ebff7d26eef87ebe99090565d46647476b94
FadelAhmed313 <146953610+FadelAhmed313@users.noreply.github.com>
2024-04-03 23:39:11 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 self.addEventListener('push', (event) => {
   const options = {
     body: event.data.text(), // Get the push message text
-    icon: '/path/to/icon.png', // Set an icon for the notification
-    badge: '/path/to/badge.png', // Set a badge for the notification
+    //icon: '/path/to/icon.png', // Set an icon for the notification
+    //badge: '/path/to/badge.png', // Set a badge for the notification
   };
 
   event.waitUntil(
-    self.registration.showNotification('My Web App', options)
+    self.registration.showNotification('ChandaApp', options)
   );
 });
